eb1bdb44b65b54cff51293aecabc7402d024e92e
lyjdwh <1412511544@qq.com>
2016-05-24 12:38:23 +0800

    finished+1

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 #约自习平台
 #模板文件(.html)放在templates中
 #在templates同级建立static文件夹，并将css,js 图片等放在static中
+#请先配置config.py中的SQLALCHEMY_DATABASE_URI 邮箱的配置
+#tmp文件用来放日志
 ## Virtual Environment
     $pip install virtualenv
     $ virtualenv --no-site-packages venv
@@ -11,3 +13,6 @@
     $ pip install -r requirment.txt
 ## Run
     $ python run.py
+
+
+
--- config.py
+++ config.py
@@ -4,14 +4,14 @@ SECRET_KEY = 'you-will-never-guess'
 
 
 
-SQLALCHEMY_DATABASE_URI='mysql+pymysql://root:abc201314@localhost/lyjdwh'
+SQLALCHEMY_DATABASE_URI='mysql+pymysql://root:abc201314@localhost/lyjdwh'#mysql的配置
 SQLALCHEMY_TRACK_MODIFICATIONS=True
 
-MAIL_SERVER='smtp.qq.com'
+MAIL_SERVER=''#邮箱服务器
 MAIL_PORT=465
 MAIL_USE_SSL=True
-MAIL_USERNAME='1412511544'
-MAIL_PASSWORD='hhvrqpgielyhggab'
+MAIL_USERNAME=''#如果是qq邮箱则为qq号，136邮箱同理
+MAIL_PASSWORD=''#客户端密码
 MAIL_USE_TLS = False
 ADMINS=['1412511544@qq.com']
 
